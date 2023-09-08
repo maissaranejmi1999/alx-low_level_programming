5d753c4a0985784011f7482811c32c63431bc2a7
maissaranejmi1999 <maissaranejmi1999>
2023-09-07 17:14:58 -0700

    2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-/* C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. */
-
 int main(void){
 
 	puts("\"Programming is like building a multilingual puzzle\n");

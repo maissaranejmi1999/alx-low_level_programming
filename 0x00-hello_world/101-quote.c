25c2f7042577f923449f47763bf3384457883f89
maissaranejmi1999 <maissaranejmi1999>
2023-09-07 18:32:24 -0700

    all

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints a quotqtion without using puts or printf
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	char dora[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,dora,59);
+	
+	return (1);
+}

155d85c7de7078e6f215b2e9ea3806c548bf8376
maissaranejmi1999 <maissaranejmi1999>
2023-09-08 20:11:25 -0700

    9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that writes alphabet
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+		putchar('0' + i);
+}
+	putchar('\n');
+	return (0);
+}

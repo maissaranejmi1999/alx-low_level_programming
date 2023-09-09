ed1d2b579495b817dd22b41dbc2ddd8e1a67142d
maissaranejmi1999 <maissaranejmi1999>
2023-09-08 18:24:26 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program that gives the syghn of the number
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+
+		printf("%d is positive", n);
+
+	} else if (n < 0)
+	{
+
+		printf("%d is negative", n);
+
+	} else if (n == 0)
+	{
+
+		printf("%d is zero", n);
+
+	}
+
+	return (0);
+}

4216a4a35668c683c55d09d32f0c231d65c9c5a2
maissaranejmi1999 <maissaranejmi1999>
2023-09-08 19:36:45 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that writes alphabet
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+		printf("%d\n", i);
+}
+	return (0);
+}

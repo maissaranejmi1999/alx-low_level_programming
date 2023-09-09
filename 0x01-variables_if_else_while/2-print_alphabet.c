5b408d9ae7f39ef9b0115a85cd69a608607246a5
maissaranejmi1999 <maissaranejmi1999>
2023-09-08 19:09:42 -0700

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that types alphabet
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char l, new;
+
+	new = '\n';
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+
+	putchar(new);
+
+
+
+}

58500b7395b00401eb02a42d358d541e66b6dd0c
maissaranejmi1999 <maissaranejmi1999>
2023-09-12 15:30:46 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: C frogram
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+
+	int i;
+	char put[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+		_putchar(put[i]);
+
+	return (0);
+
+}

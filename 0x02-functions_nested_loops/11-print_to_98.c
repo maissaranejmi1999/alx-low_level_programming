0bb73e4501b2aedadb1ee0b7fcf7ec00b94d0d52
maissaranejmi1999 <maissaranejmi1999>
2023-09-12 19:32:56 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,16 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n <= 97; n++)
 		{
 			printf("%d, ", n);
 		}
 	} else if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n >= 97; n--)
 		{
 			printf("%d, ", n);
 		}
 	}
+	printf("98\n");
 }

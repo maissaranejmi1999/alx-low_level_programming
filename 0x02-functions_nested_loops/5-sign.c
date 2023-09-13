72723a50f614383c0d7954355357c3252e6e472d
maissaranejmi1999 <maissaranejmi1999>
2023-09-12 17:44:40 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry
+ *
+ * @n: checks input of function
+ *
+ * Return: 0 or 1
+*/
+
+int print_sign(int n);
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	if (n == 0)
+		_putchar('0');
+		return (0);
+	_putchar('-');
+	return (-1);
+}
+

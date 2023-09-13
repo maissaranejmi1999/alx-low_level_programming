11ac7ee0dd86943c9dcc65267fba0cfd408526fb
maissaranejmi1999 <maissaranejmi1999>
2023-09-12 19:59:12 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Description: C frogram
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0)
+		{
+			k = k + i;
+		}
+
+		printf("%d", k);
+
+	}
+}

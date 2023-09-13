db319ff19af67a2255bd3108ddc261595c795d6f
maissaranejmi1999 <maissaranejmi1999>
2023-09-12 19:29:15 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: variable
+ *
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	} else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+}

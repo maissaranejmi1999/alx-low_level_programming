5b1eb94f66c455e1485e48dba10b4a3b55d7e3fc
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 16:25:02 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function
+ *
+ * Description: checks is c is uppercase letter
+ *
+ * @n: variable
+*/
+
 void print_diagonal(int n)
+{
+	int j, i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (j == i)
+					_putchar(92);
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}

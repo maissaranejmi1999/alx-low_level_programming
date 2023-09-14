b70939a5b64326d632d814ccfb686eb54f2cd643
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 14:33:26 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,22 @@
 #include "main.h"
 
 /**
- * print_numbers - function
+ * print_most_numbers - function
  *
  * Description: checks is c is uppercase letter
 */
 
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		} else
+			_putchar(i);
+	}
+	_putchar('\n');
+}

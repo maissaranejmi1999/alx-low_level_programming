e8a6bdd8515b50ebb8494fd18830df0ba21a021c
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 16:00:26 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n <= 0)
-			break;
-		_putchar(95);
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

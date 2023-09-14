917945a5c202b9d9d497bcfa027a0c048167ef64
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 16:37:36 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,28 @@
+#include "main.h"
 
+/**
+ * print_square - function
+ *
+ * Description: checks is c is uppercase letter
+ *
+ * @size: variable
+*/
 
 void print_square(int size)
 {
-	
+	int j, i;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }

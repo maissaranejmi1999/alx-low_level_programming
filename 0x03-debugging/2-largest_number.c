d9ed19848cc2b99c97229ecb66c51e3509bf1f68
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 12:36:31 -0700

    1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,6 +7,11 @@
  * @c: third integer
  * Return: largest number
  */
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+*/
+
 
 int largest_number(int a, int b, int c)
 {
@@ -26,4 +31,20 @@ int largest_number(int a, int b, int c)
     }
 
     return (largest);
+}
+
+int main(void)
+{
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
 }

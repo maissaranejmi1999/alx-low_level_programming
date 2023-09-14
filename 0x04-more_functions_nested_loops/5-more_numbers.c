248bb68e8bc31d687370a0a638640f9e2ea978f0
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 14:50:44 -0700

    4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - function
+ *
+ * Description: letter
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+}

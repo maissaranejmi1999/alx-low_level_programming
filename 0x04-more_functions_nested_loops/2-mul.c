c58bcb33b3c8241b4653d83e571d43b4c4451d66
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 14:00:40 -0700

    0

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - function
+ * _isupper - function
  *
  * Description: checks is c is uppercase letter
  *
@@ -10,12 +10,4 @@
  * Return: 1 if upper
 */
 
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	} else
-		return (0);
-	return (0);
-}
+

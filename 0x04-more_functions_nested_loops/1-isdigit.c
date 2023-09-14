a9b8555af62d47e449a4ea42936971c9cec01a9e
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 13:54:56 -0700

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - function
+ *
+ * Description: checks is c is uppercase letter
+ *
+ * @c: is a variable
+ *
+ * Return: 1 if upper
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	} else
+		return (0);
+	return (0);
+}

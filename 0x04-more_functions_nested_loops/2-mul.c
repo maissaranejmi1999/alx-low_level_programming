ba2aca4102fa6b231e1eec83ecb27acc0b27e6b7
maissaranejmi1999 <maissaranejmi1999>
2023-09-14 14:06:03 -0700

    0

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - function
+ * mul - function
  *
  * Description: checks is c is uppercase letter
  *
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ *
+ * Description: checks is c is uppercase letter
+*/
+
+void print_numbers(void)
+{
+
+}

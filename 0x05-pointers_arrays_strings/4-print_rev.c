5a818794f34e078e5af5afa8d6e05e03b5ee09a8
maissaranejmi1999 <maissaranejmi1999>
2023-09-18 16:47:41 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ *
+ * @s : variabe
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i])
+	{
+		i++
+	}
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

fd5d33ca7a94d1240b3183faa6dfc4eb5b4f5494
maissaranejmi1999 <maissaranejmi1999>
2023-09-19 15:52:16 -0700

    7

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
-
+/**
+ * print_array - function
+ * @a : an array
+ * @n : a variable
+*/
 
 void print_array(int *a, int n)
 {
+	int i = 0;
 
+	while (i <= n)
+	{
+		if (i != n)
+		{
+			_putchar(a[i]);
+			_putchar(',');
+			_putchar(' ');
+		} else if (i == n)
+		{
+			_putchar(a[i]);
+			_putchar('\n');
+		}
+	}
 }

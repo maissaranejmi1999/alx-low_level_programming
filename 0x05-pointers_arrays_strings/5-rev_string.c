d78b1f8b430aafda1ed22fa6f5cbaa809e860d1f
maissaranejmi1999 <maissaranejmi1999>
2023-09-18 17:00:02 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function
+ *
+ * @s : variabe
+*/
+
+void rev_string(char *s)
+{
+		int i, j, k;
+		char *t;
+
+	while (s[i])
+	{
+		i++
+	}
+
+	k = i;
+
+	for (j = 0; i >= 0 && j <= k; i--)
+	{
+		s[i] = t[j];
+		j++;
+	}
+}

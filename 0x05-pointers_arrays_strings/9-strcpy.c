e526771a2fe1ed2b39e7d727acd901e1f07a8b30
maissaranejmi1999 <maissaranejmi1999>
2023-09-19 16:39:07 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
-
+/**
+ * *_strcpy - function
+ * @dest: an array
+ * @src: a variable
+ * Return: a char
+*/
 
 char *_strcpy(char *dest, char *src)
 {
-	
+	int j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++
+	}
+	return (dest);
 }

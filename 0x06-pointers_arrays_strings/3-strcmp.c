f3257c4963b3ae09a39bcae4a8aa345dfc9dc775
maissaranejmi1999 <maissaranejmi1999>
2023-09-21 16:53:38 -0700

    2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,17 @@
  * _strcmp - concatenates
  * @s1: string
  * @s2: string
- * Return : int
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if ((*s1 - *s2) != 0)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}

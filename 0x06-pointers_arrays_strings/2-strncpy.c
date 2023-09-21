a81582d00f109e84635a12f082443c626e1abda3
maissaranejmi1999 <maissaranejmi1999>
2023-09-21 16:12:46 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates
+ * @dest: string
+ * @src: string
+ * @n: bytes
+ *
+ * Return: string concatenated
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+	dest[i] = '\0';
+	return (0);
+}

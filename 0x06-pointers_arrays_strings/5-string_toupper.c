cf655bdd6b9230a5ac91e9b352cfc4c7f32a11f1
maissaranejmi1999 <maissaranejmi1999>
2023-09-21 18:23:31 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,20 @@
 #include "main.h"
 
+/**
+ * string_toupper - function
+ * @x: variable
+ * Return: x
+*/
 
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
-	
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		if (x[i] >= 61 && x[i] <= 122)
+			x[i] = x[i] - 20;
+		i++;
+	}
+	return (x);
 }

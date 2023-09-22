47e93ab86072500289a92209222a58b900966364
maissaranejmi1999 <maissaranejmi1999>
2023-09-21 19:14:49 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,15 @@ char *leet(char *x)
 	while (x[i] != '\0')
 	{
 		if (x[i] == 'a' || x[i] == 'A')
-				x[i] = '3';
+			x[i] = '4';
+		if (x[i] == 'e' || x[i] == 'E')
+			x[i] = '3';
+		if (x[i] == 'o' || x[i] == 'O')
+			x[i] = '0';
+		if (x[i] == 't' || x[i] == 'T')
+			x[i] = '7';
+		if (x[i] == 'l' || x[i] == 'L')
+			x[i] = '1';
 
 		i++;
 	}

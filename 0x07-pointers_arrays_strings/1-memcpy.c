f9d429e955b12837fef1e48346ee07ccab7d8542
maissaranejmi1999 <maissaranejmi1999>
2023-09-25 15:51:37 -0700

    0

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
+/**
+ * _memcpy - function
+ * @dest: variable
+ * @src: variable
+ * @n: variable
+ * Return: l
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	char *l = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (l);
+}

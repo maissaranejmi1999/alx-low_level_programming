a4caa23630cb12085eb968e78ad18bf8303dd321
maissaranejmi1999 <maissaranejmi1999>
2023-09-25 16:47:09 -0700

    0

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ * @s: string
+ * @c: string
+ * Return: (Success) first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *l = s;
+
+	if (s == NULL)
+		return (NULL);
+	else if (s != NULL)
+	{
+		for (l = s; *l; l++)
+			if (*l == c)
+				return (l);
+	}
+	return (l);
+}

89d3c9319172df69cfcbc63aaa64b26e2bbbdad3
maissaranejmi1999 <maissaranejmi1999>
2023-10-10 16:36:49 -0700

    0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -7,6 +6,7 @@
  * @name: variable
  * @age: variable
  * @owner: variable
+ * Return: void
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)

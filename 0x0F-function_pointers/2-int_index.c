5b4597ae2fbfe2d44072ca6c326b5693812adf74
maissaranejmi1999 <maissaranejmi1999>
2023-10-11 15:44:55 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function
+ * @array: varible
+ * @size: variable
+ * @cmp: variable
+ * Return: int
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i] != 0))
+				{
+					return (i);
+				}
+			}
+			return (-1);
+		} else
+			return (-1);
+	}
+	return (0);
+}

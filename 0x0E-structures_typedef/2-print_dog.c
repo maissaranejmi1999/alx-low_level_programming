ec78fc0fea69a9b38b000785bfc773063b6a5ce0
maissaranejmi1999 <maissaranejmi1999>
2023-10-10 17:37:45 -0700

    0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,20 +12,18 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-		{
-			printf("Name: %s\n", (*d).name);
-		} else
-		{
-			printf("Name: (nil)");
-		}
-		printf("Age: %f\n", (*d).age);
-		if (d->owner)
-		{
-			printf("Owner: %s\n", (*d).owner);
-		} else
-		{
-			printf("Owner: (nil)");
-		}
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 
+		if (d->age >= 0)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }

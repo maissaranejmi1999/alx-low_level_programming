278f1b033dfdb479423a7235e5f2b9b4a3515027
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 18:38:58 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,18 +13,16 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if ((new_node == NULL) || (head == NULL))
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
-	
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	
-	return new_node;
+	return (new_node);
 }

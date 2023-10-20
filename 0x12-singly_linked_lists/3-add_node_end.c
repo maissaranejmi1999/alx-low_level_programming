4a783d22b4ecc6dc147456a35c05c88fe4a52b82
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 18:45:00 -0700

    2

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node_ent - function
+ * @head: double pointer to the first element of a linked list
+ * @str: string
+ * Description: adds a new node at the end of linked list
+ * Return: address
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if ((new_node == NULL) || (head == NULL))
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	*head = new_node;
+	return (new_node);
+}

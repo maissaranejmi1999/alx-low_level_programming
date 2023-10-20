63a10f625aea24ea0af2433a6e55a8cdd38bd6d9
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 19:21:47 -0700

    2

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,19 +4,18 @@
  * free_list - function
  * @head: double pointer to head of first linked list
  * Description: FREES linked list
- * Return: null
  */
-listint_t free_list(listint_t **head)
+
+void free_list(list_t **head)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = head;
 	int i = 0;
 
 	if (*head)
+	while (new_node != NULL)
 	{
-		while (new_mode->next != NULL)
-			new_mode = new_mode->next;
-			free(new_node);
-			i++;
+		new_node = new_mode->next;
+		free(new_node->str);
+		i++;
 	}
-	return (NULL);
 }

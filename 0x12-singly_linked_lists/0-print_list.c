4a708c169d22caa1cab0b69793c690beff6235c2
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 17:06:44 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -15,7 +14,8 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}

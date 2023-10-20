5e7d909832fd55210b9322cc1789ce7c46a00257
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 19:36:20 -0700

    2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,10 +25,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-	} else 
+	} else
 	{
 		list_t *cnode = *head;
-		while (*head != NULL)
+
+		while (cnode->next != NULL)
 		{
 			cnode = cnode->next;
 		}

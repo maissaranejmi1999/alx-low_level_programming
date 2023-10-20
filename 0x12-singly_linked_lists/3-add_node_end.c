91f2030c9da2fbb09e4ca8d42b9b421a026af0f3
maissaranejmi1999 <maissaranejmi1999>
2023-10-19 19:51:16 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,32 +7,31 @@
  * Description: adds a new node at the end of linked list
  * Return: address
  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
 
-list_t *add_node_end(list_t **head, const char *str) {
-    if (str == NULL)
-        return NULL;
+	if (new_node == NULL || head == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	} else
+	{
+		list_t *cnode = *head;
 
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return NULL;
-    }
-
-    new_node->next = NULL;
-
-    if (*head == NULL) {
-        *head = new_node;
-    } else {
-        list_t *current = *head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = new_node;
-    }
-
-    return new_node;
+		while (cnode->next != NULL)
+		{
+			cnode = cnode->next;
+		}
+		cnode->next = new_node;
+	}
+	return (new_node);
 }

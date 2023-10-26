14094aa51b08049c663a27d261a8484b347eef6f
maissaranejmi1999 <molinmay1999@gmail.com>
2023-10-25 17:01:52 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function
+ * @s: chane of caracter
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		write(1, "\n", 1);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	write(1, s, 1);
+}

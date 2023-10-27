9a4322db880e5f4a308ba03c17fae3dc9ccc4587
maissaranejmi1999 <molinmay1999@gmail.com>
2023-10-27 04:23:57 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		num = atoi(argv[i]);
-		if (num == 0 && argv[i][0] != '0')
+		if ((num == 0 && argv[i][0] != '0') || num < 0)
 		{
 			printf("Error\n");
 			return (1);

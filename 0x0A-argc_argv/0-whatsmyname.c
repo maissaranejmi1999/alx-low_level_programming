f363edd994e0f0adf7b3a5ed9e7d6d93b41c5661
maissaranejmi1999 <molinmay1999@gmail.com>
2023-10-27 02:18:13 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * @argc: number of argumrnts
+ * @argv: pointer array
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

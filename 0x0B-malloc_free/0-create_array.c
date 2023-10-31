d5913898d2620e84247088991f932d78caba83cb
maissaranejmi1999 <molinmay1999@gmail.com>
2023-10-31 00:10:47 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *ar = (char *)malloc((size + 1) * sizeof(char));
+	char *ar = (char *)malloc(size * sizeof(char));
 
 	if (ar == NULL || size == 0)
 		return (NULL);

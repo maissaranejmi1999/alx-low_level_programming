16604418d169dd139c0f862c9cee5295ba3f015e
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-03 01:30:57 -0700

    100

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	char *char_ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,10 +19,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	char_ptr = ptr;
-
 	while (nmemb--)
-		*char_ptr++ = 0;
+	{
+		((char *)ptr)[nmemb] = 0;
+	}
 
 	return (ptr);
 }

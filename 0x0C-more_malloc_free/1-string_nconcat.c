d2ee12aa0fc2cfaa3e4d881b304908a50901b7b8
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-03 02:32:13 -0700

    100

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i = 0, j = 0, k = 0;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)

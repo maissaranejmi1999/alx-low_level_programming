ff6a99f0fd09e28fdebde68de5f1c13653ab5b76
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-08 08:36:52 -0800

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,19 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: pointer
+ * @size: the size
+ * @action: function pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (array && size && action)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}

26bf812aea60cc8c8045813f85a6a47dea582d96
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-16 17:07:06 -0800

    m

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    hello_msg db 'Hello, Holberton', 0
+    format db '%s', 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rdi, format
+    mov rsi, hello_msg
+    call printf
+    pop rbp
+    ret

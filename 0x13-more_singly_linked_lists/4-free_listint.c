519289600a4ec96ed369548763922dd2b359484f
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-21 04:47:57 -0800

    m

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - function
+ * @head: pointer
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t x;
+	while (head != NULL)
+		x = head->next;
+		free(head->n);
+		free(head);
+		head = x;
+}

90d6d075f9b79bcebf97f374c49ab7bdbe2d500d
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-21 14:10:23 -0800

    m

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,12 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *x;
 
-	x = *head;
+	x = head;
+	if (index == 0)
+		return (head);
 	while (x->next != NULL)
 	{
 		if (i == index)

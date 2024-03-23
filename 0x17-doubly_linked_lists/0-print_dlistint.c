bf773f3fc9551d5558492232448cee1dcd570463
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-23 09:34:45 -0700

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ *
+ * Description: prints all the elements of a dblk list
+ *
+ * @h: head of list
+ *
+ * Return: size_t.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *nd;
+
+	printf("%d\n", h->n);
+	nd->prev = h;
+	while (nd->next != NULL)
+	{
+		printf("%d\n", nd->n);
+		nd->next = nd;
+	}
+}

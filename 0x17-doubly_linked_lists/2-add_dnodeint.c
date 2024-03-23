4d360f73fc39d377f3cef1d84f87125b751b663d
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-23 10:25:22 -0700

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - check the code
+ *
+ * Description: prints all the elements of a dblk list
+ *
+ * @h: head of list
+ *
+ * Return: size_t.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nd = *head;
+
+	*head->n = n;
+	*head->next = nd;
+	*head->prev = NULL;
+
+	return (*head);
+}

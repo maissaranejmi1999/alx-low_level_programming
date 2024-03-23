c2c6b4ac164d0a7b52498834b23b6190ecb0f0bc
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-23 09:59:48 -0700

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,8 +13,9 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *nd = h;
-	size_t i = 0;
+	size_t i = 1;
 
+	printf("%d\n", nd->h)
 	while (nd->next != NULL)
 	{
 		printf("%d\n", nd->n);
